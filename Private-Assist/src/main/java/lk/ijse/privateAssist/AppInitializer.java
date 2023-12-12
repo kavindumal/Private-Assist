b4ca301e6d31c3908ae683e46857c466f6cf1b0e
kavindumal <kavindu11250403@gmail.com>
2023-12-12 13:50:09 +0530

    Private-Assist version 0.003

--- Private-Assist/src/main/java/lk/ijse/privateAssist/AppInitializer.java
+++ Private-Assist/src/main/java/lk/ijse/privateAssist/AppInitializer.java
@@ -2,8 +2,6 @@ package lk.ijse.privateAssist;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
-import javafx.scene.ImageCursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -19,12 +17,12 @@ public class AppInitializer extends Application {
     public void start(Stage stage) throws Exception {
 //        Image cursorImage = new Image("img/icons/cursor2.png");
 //        Cursor customCursor = new ImageCursor(cursorImage);
-        Parent rootNode = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/loginForm.fxml")));
+        Parent rootNode = FXMLLoader.load(Objects.requireNonNull(this.getClass().getResource("/view/welcomeForm.fxml")));
 //        rootNode.setCursor(customCursor);
         Scene scene = new Scene(rootNode);
 //        scene.setCursor(customCursor);
         Image icon = new Image("/img/logo/logo.png");
-
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Private Assist");
         stage.getIcons().add(icon);
         stage.setScene(scene);
--- Private-Assist/src/main/java/lk/ijse/privateAssist/controller/LoginFormController.java
+++
@@ -1,4 +0,0 @@
-package lk.ijse.privateAssist.controller;
-
-public class LoginFormController {
-}
